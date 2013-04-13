600ede591e39e3ec9ce58783b0cbdc15e36628d4
g-goessel <g_goessel@outlook.com>
2013-04-13 16:28:36 +0200

    all resotred - works fine now

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,26 +2,6 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-<<<<<<< HEAD
-  describe "Home page" do
-
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
-  end
-
-=======
   subject { page }
 
   describe "Home page" do
@@ -52,5 +32,4 @@ describe "Static pages" do
     it { should have_content('Contact') }
     it { should have_title(full_title('Contact')) }
   end
->>>>>>> filling-in-layout
 end
