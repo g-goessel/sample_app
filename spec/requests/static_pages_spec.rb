6171d65399a202ee5bd236a076eabe9c985f3a65
g-goessel <g_goessel@outlook.com>
2013-04-13 11:17:51 +0200

    layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,7 @@ describe "Static pages" do
     before { visit root_path }
 
     it { should have_content('Sample App') }
-    it { should have_title('') }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
 
@@ -16,20 +16,20 @@ describe "Static pages" do
     before { visit help_path }
 
     it { should have_content('Help') }
-    it { should have_title('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
     it { should have_content('About') }
-    it { should have_title('About Us') }
+    it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
     it { should have_content('Contact') }
-    it { should have_title('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
